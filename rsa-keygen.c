a238fa192e6c21dc237eeebbac16d5df89ec6231
Jackson Collier <jacksontcollier@gmail.com>
2017-11-08 21:34:06 -0500

    Complete rsa-keygen

--- rsa-keygen.c
+++ rsa-keygen.c
@@ -3,7 +3,17 @@
 int main(int argc, char** argv)
 {
   RSAKeygenOptions* keygen_options = parse_RSAKeygenOptions(argc, argv);
-  keygen_options->print(keygen_options);
+  RSAKey* rsa_key = gen_RSAKey(keygen_options->num_bits);
+
+  FILE* public_key_out = fopen(keygen_options->public_key_file, "w");
+  print_RSAKey(public_key_out, rsa_key->num_bits, BN_bn2dec(rsa_key->N),
+               BN_bn2dec(rsa_key->e));
+  fclose(public_key_out);
+
+  FILE* secret_key_out = fopen(keygen_options->secret_key_file, "w");
+  print_RSAKey(secret_key_out, rsa_key->num_bits, BN_bn2dec(rsa_key->N),
+               BN_bn2dec(rsa_key->d));
+  fclose(secret_key_out);
 
   return 0;
 }
